e8d7ae287587cf437d62135bf96f104e0a6c425f
Diana Buras <burasdiana@hotmail.com>
2019-03-15 22:41:50 +0100

    [add] reverse string

--- Samples.cs
+++ Samples.cs
@@ -18,3 +18,27 @@ class MainClass {
   } 
    
 }
+
+
+// return the reverse of a string
+
+using System;
+
+class MainClass {
+  public static string FirstReverse(string str) { 
+  
+		string res = "";
+		for (int i = str.Length - 1; i>= 0; i--){
+			res = res + str[i];
+		}
+    return res;
+            
+  }
+
+  static void Main() {  
+    // keep this function call here
+    Console.WriteLine(FirstReverse(Console.ReadLine()));
+  } 
+   
+}
+
